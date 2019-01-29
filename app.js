d8358a0e9db8ccb60d74cbcb4dbd86681227647f
Vermont Garcia <vermontgarcia@hotmail.com>
2019-01-29 17:22:32 -0600

    Fixing running errors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const cors         = require('cors');
 
 /*
 mongoose
@@ -31,6 +32,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+//Cors
+
+app.use(cors({
+  origin: ['http://localhost:3000','https://messagesanitizer.herokuapp.com']
+}));
+
+
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
@@ -43,7 +51,7 @@ app.use(require('node-sass-middleware')({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 
@@ -54,6 +62,11 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index');
 app.use('/', index);
+
+// Integrating REACT app into REST API files
+app.all('*', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+})
 
 
 module.exports = app;
