2224541d5f346610c30419e84aee6c398910703f
Vermont Garcia <vermontgarcia@hotmail.com>
2019-01-29 19:29:36 -0600

    DB connected, auth routes implemented

--- app.js
+++ app.js
@@ -10,16 +10,14 @@ const logger       = require('morgan');
 const path         = require('path');
 const cors         = require('cors');
 
-/*
 mongoose
-  .connect('mongodb://localhost/sanitizer', {useNewUrlParser: true})
+  .connect(process.env.DB, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
   .catch(err => {
     console.error('Error connecting to mongo', err)
   });
-*/
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -51,7 +49,7 @@ app.use(require('node-sass-middleware')({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 
 
@@ -61,7 +59,10 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 const index = require('./routes/index');
+const authRouter = require('./routes/auth')
+
 app.use('/', index);
+app.use('/api/auth', authRouter);
 
 // Integrating REACT app into REST API files
 app.all('*', (req, res) => {
